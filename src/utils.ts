e0e1b8900b9cb550019f8df80a1a498c88000d07
Mohammed Alamri <modev32@gmail.com>
2022-07-06 12:01:26 +0300

    Had to replace old function with my code that I hope is working solid

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,54 @@
 /**
- * Debouncing passed in functions to avoid multiple triggers.
- * @param {Function} func The function to debounce.
- * @param {number} wait The time to wait in milliseconds.
- * @returns {Function} The debounced function.
- */ 
-export function debouncer<T extends Function>(func: T, wait: number = 0) {
-  let timeout: NodeJS.Timeout = null;
-  const callable = (...args: any) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func(...args), wait);
-  };
-  return <T>(<any>callable);
+ * Extension events limiter.
+ * This class is responsible for managing  the  events of the extension to
+ * avoid duplicated events, because the debouncing method didn't work well.
+ * First we initilize EventsLimit with desired limit in ms and then we register
+ * the function to be fired one time immediatly(leading) or the last (trailing)
+ * until the timeout is over.
+ */
+export class EventsLimit {
+  private tailTimer: number = undefined;
+  private leadTimer: number = undefined;
+  private isCalled: boolean = false;
+  private func: Function = undefined;
+  private debounce: ReturnType<typeof setTimeout>;
+
+  /**
+   * EventsLimit constructor.
+   * @param trailTimer The limit in milliseconds for the trailing event.
+   * @param leadTimer The limit in milliseconds for the leading event.
+   */
+  constructor (_trailTimer: number = 100, _leadTimer: number = 200) {
+    this.tailTimer = _trailTimer;
+    this.leadTimer = _leadTimer;
+  }
+
+  /**
+* Because some events like onDidChangeActiveTextEditor get fired twice when
+* an editor tab is opened. The first event is for the visible range of pre
+* mouse position and then gets another event but with the correct visible range.
+* So we will use this method to fire the last passed event.
+*/
+  public Trail = () => {
+    clearTimeout(this.debounce);
+    this.debounce = setTimeout(() => this.func(), this.tailTimer);
+  }
+
+  private reset = () => {
+    return setTimeout(() => {
+      this.isCalled = false;
+    }, this.leadTimer);
+  }
+
+  public Lead = () => {
+    if (!this.isCalled) {
+      this.isCalled = true;
+      this.func();
+    }
+    this.reset();
+  }
+
+  public Register = (func: Function) => {
+    this.func = func;
+  }
 }
