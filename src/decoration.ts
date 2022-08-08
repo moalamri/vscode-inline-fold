c19a64f88a129ecff2d72cf8ef28ab3da1bb23b4
Mohammed Alamri <modev32@gmail.com>
2022-08-09 01:50:33 +0300

    Added comments

--- src/decoration.ts
+++ src/decoration.ts
@@ -2,29 +2,38 @@ import { DecorationInstanceRenderOptions, DecorationOptions, DecorationRangeBeha
 import { ExtensionConfigs } from "./config";
 import { Configs } from "./enums";
 
-
+/**
+ * With each time the decorator is triggered, and the method setDecoration is called,
+ * The extension will create a new decoration type with sequential numbers. This will
+ * Create large number of decoration types which will overlapse each other.
+ * This class DecoratorTypeOptions is used to cache the decoration type for each trigger
+ * To only have single applied set of decoration type per language id.
+ * on demand.
+ */
 export class DecoratorTypeOptions {
   private cache = new Map<string, TextEditorDecorationType>();
+  private lineChahe: string[];
 
   public ClearCache() {
     this.cache.clear();
+    delete this.lineChahe;
   }
 
-  public UnfoldedDecorationType = (langId: string): DecorationRenderOptions => {
+  public UnfoldedDecorationType = (langId: string /* To use later for lang scoped configs */): DecorationRenderOptions => {
     return {
       rangeBehavior: DecorationRangeBehavior.ClosedClosed,
-      opacity: ExtensionConfigs.get<string>(Configs.unfoldedOpacity, langId).toString()
+      opacity: ExtensionConfigs.get<string>(Configs.unfoldedOpacity).toString()
     }
   }
 
-  public MatchedDecorationType = (langId: string): DecorationRenderOptions => {
+  public MatchedDecorationType = (langId: string /* To use later for lang scoped configs */): DecorationRenderOptions => {
     return {
       before: {
-        contentText: ExtensionConfigs.get<string>(Configs.maskChar, langId),
-        color: ExtensionConfigs.get<string>(Configs.maskColor, langId),
+        contentText: ExtensionConfigs.get<string>(Configs.maskChar),
+        color: ExtensionConfigs.get<string>(Configs.maskColor),
       },
       after: {
-        contentText: ExtensionConfigs.get<string>(Configs.after, langId),
+        contentText: ExtensionConfigs.get<string>(Configs.after)
       },
       letterSpacing: "-2ch",
       textDecoration: "none; display: none;"
@@ -44,13 +53,13 @@ export class DecoratorTypeOptions {
   }
 
   public PlainDecorationTypeCache(langId: string): TextEditorDecorationType {
-    return this.cache.has(langId) ? this.cache.get(langId):
+    return this.cache.has(langId) ? this.cache.get(langId) as TextEditorDecorationType: 
       this.cache.set(langId, window.createTextEditorDecorationType(this.PlainDecorationType()))
         .get(langId);
   }
 
   public MatchedDecorationOptions = (range: Range, _languageId: string): DecorationOptions => {
-    const configs = ExtensionConfigs.get<DecorationInstanceRenderOptions>(Configs.decorationOptions, _languageId);
+    const configs = ExtensionConfigs.get<DecorationInstanceRenderOptions>(Configs.perLanguageOptions);
     return {
       range,
       renderOptions: configs
@@ -67,7 +76,6 @@ export class DecoratorTypeOptions {
   public PlainDecorationType = (): DecorationRenderOptions => {
     return { rangeBehavior: DecorationRangeBehavior.ClosedClosed }
   }
-
 
   constructor () { }
 
