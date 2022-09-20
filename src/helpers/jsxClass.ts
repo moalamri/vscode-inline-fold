bd7bc65e0bab3899162bb216a8f293cc4656f32c
Mohammed Hassan <modev32@gmail.com>
2022-09-20 01:52:12 +0000

    Code documentation

--- src/helpers/jsxClass.ts
+++ src/helpers/jsxClass.ts
@@ -53,34 +53,44 @@ export class LineNavigate {
     this.index = 0;
   }
 
+  // Move a step ahead and return the next character (Current)
   next() {
     this.index++;
     return this.line.charAt(this.index);
   }
 
+  // Move a step back and return the previous character (Current)
   prev() {
     this.index--;
     return this.line.charAt(this.index);
   }
 
+  // Get the current chart in the current location
   curr() {
     return this.line.charAt(this.index);
   }
 
+  // Check if the current passed line has an attribute or a string in `Pre` configuration
   hasPre() {
     return this.line.includes('className=');
   }
 
+  // Get the index of the character that follows Pre configuration.
+  // `className=`[index] <-- should be the `Start` character
   indexAfterPre() {
     this.index = this.line.indexOf('className=') + 'className='.length;
     return this.index
   }
 
-  at(i) {
+  // Return the character at specific location without changing the index
+  at(i: number) {
     return this.line.charAt(i)
   }
 
-  passThis(char) {
+  /** Usefull to skip some characters like whitespace or so.
+  /* @param {char} character to skip
+  */
+  passThis(char: string) {
     if (this.curr() === char) {
       this.next();
       return true;
@@ -89,9 +99,11 @@ export class LineNavigate {
     }
   }
 
+  // Return the character of the next index without changing current walk
   peekNext() {
     return this.line.charAt(this.index + 1);
   }
 
+  // TODO
   lookForAheah(type) {}
 }
