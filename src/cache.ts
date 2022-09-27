a2cce92af531f9835f001aed90ec16c697f8a9f9
Mohammed Alamri <modev32@icloud.com>
2022-09-27 08:59:02 +0300

    Code documenting

--- src/cache.ts
+++ src/cache.ts
@@ -10,21 +10,25 @@ class CacheClass {
   }
   StateCache: Map<string, boolean> = new Map<string, boolean>();
 
+  // Get the autoFold setting
   autoFold() {
     return ExtSettings.Get<boolean>(Settings.autoFold);
   }
 
+  // Set the initial state of the extension
   init() {
     this.State = this.autoFold();
   }
 
+  // Set the state of the extension
   set State(_state: boolean) {
     this.StateCache.set(window.activeTextEditor?.document.uri.path, _state);
   }
 
+  // Get the state of the extension
   get State(): boolean {
     const state = this.StateCache.get(window.activeTextEditor?.document.uri.path);
-    if(state === undefined) {
+    if (state === undefined) {
       this.init();
       return this.State;
     } else {
@@ -32,16 +36,18 @@ class CacheClass {
     }
   }
 
+  // Toggle the state of the extension
   public ToggleState() {
     this.State = !this.State;
   }
 
+  // Clear the state cache
   public ClearCache() {
     this.StateCache.clear();
   }
 
-  constructor() {}
+  constructor () { }
 }
 
-// We will use singleton pattern to make sure that we only have one instance of the class
+// We will use singleton pattern to make sure that we only have one instance of the state
 export const Cache = CacheClass.Instance;
