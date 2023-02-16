174a093e4fc60b36946f2c243a61b2bb3030342c
Mohammed Alamri <modev32@gmail.com>
2023-02-16 18:20:32 +0300

    Add `togglePerFile` key to setting's enums

--- src/enums.ts
+++ src/enums.ts
@@ -14,5 +14,6 @@ export enum Settings {
    after = "after",
    supportedLanguages = "supportedLanguages",
    unfoldOnLineSelect = "unfoldOnLineSelect",
-   useGlobal = "useGlobal"
+   useGlobal = "useGlobal",
+   togglePerFile = "togglePerFile"
 }
